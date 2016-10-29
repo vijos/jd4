6f47c11a51e7e40944df8bdedefe57590e7d09f2
iceboy <me@iceboy.org>
2016-10-28 21:58:34 -0700

    add asio

--- main.cc
+++ main.cc
@@ -1,7 +1,9 @@
 #include <gflags/gflags.h>
 #include <glog/logging.h>
+#include <boost/asio.hpp>
 
 int main(int argc, char *argv[]) {
   google::InitGoogleLogging(argv[0]);
   gflags::ParseCommandLineFlags(&argc, &argv, true);
+  boost::asio::io_service io_service;
 }
