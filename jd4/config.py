919e64bddd429aafb8975233a54e22aed6d7c447
Wende Tan <twd2@163.com>
2017-03-12 01:16:33 +0800

    server_url is configurable

--- jd4/config.py
+++ jd4/config.py
@@ -5,17 +5,22 @@ from functools import partial
 from os import path
 from ruamel import yaml
 
+from jd4.log import logger
+
 _CONFIG_DIR = user_config_dir('jd4')
 _CONFIG_FILE = path.join(_CONFIG_DIR, 'config.yaml')
 _COOKIES_FILE = path.join(_CONFIG_DIR, 'cookies')
 
 def _load_config():
+    if not path.exists(_CONFIG_FILE):
+        logger.error('Config file %s not found.', _CONFIG_FILE)
+        exit(1)
     with open(_CONFIG_FILE) as file:
         return yaml.load(file, Loader=yaml.RoundTripLoader)
 
 config = _load_config()
 
-cookie_jar = CookieJar()
+cookie_jar = CookieJar(unsafe=True)
 try:
     cookie_jar.load(_COOKIES_FILE)
 except FileNotFoundError:
