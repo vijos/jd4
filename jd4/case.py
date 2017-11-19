46a5bda3d468a87e940b9c7f388c8f9e91af8d40
iceboy <me@iceboy.org>
2017-11-18 21:07:12 -0800

    remove unused import [ci skip]

--- jd4/case.py
+++ jd4/case.py
@@ -4,17 +4,13 @@ from functools import partial
 from io import BytesIO, TextIOWrapper
 from itertools import islice
 from os import mkfifo, path
-from random import randint
 from socket import socket, AF_UNIX, SOCK_STREAM, SOCK_NONBLOCK
 from zipfile import ZipFile
 
 from jd4._compare import compare_stream
-from jd4.cgroup import try_init_cgroup, wait_cgroup
-from jd4.compile import Compiler
+from jd4.cgroup import wait_cgroup
 from jd4.status import STATUS_ACCEPTED, STATUS_WRONG_ANSWER, STATUS_RUNTIME_ERROR, \
                        STATUS_TIME_LIMIT_EXCEEDED, STATUS_MEMORY_LIMIT_EXCEEDED
-from jd4.log import logger
-from jd4.sandbox import create_sandbox
 from jd4.util import read_pipe
 
 CHUNK_SIZE = 32768
